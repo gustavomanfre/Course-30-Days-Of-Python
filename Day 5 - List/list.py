b0dd56255d2d0d12821e20a549fbbc2ba8ab4da9
Gustavo Manfredi <gustavomanfredi1995@gmail.com>
2026-01-01 22:23:48 -0300

    Listo ejercicios list

--- Day 5 - List/list.py
+++ Day 5 - List/list.py
@@ -77,42 +77,148 @@ print(it_companies[0:3])
 
 #20- Eliminar la primera empresa de TI de la lista
 it_companies.pop(0)
-print(it_companies) 
+print(it_companies)
 
-#El método de remove elimina un elemento especificado de una lista
-#El método pop() elimina el índice especificado (o el último elemento si no se especifica el índice)
-#La palabra clave del elimina el índice especificado y también se puede utilizar para eliminar elementos dentro del rango de índice. También puede eliminar la lista por completo
-""""
-Eliminar la mediana empresa de TI o las empresas de la lista
+#Todos modifican la lista original.
+#REMOVE
+#El método de remove elimina un elemento especificado de una lista. remove(valor): Mutación por búsqueda
+#POP
+#El método pop() elimina el índice especificado (o el último elemento si no se especifica el índice). pop(índice): Mutación con retorno
+#DEL
+#La palabra clave del elimina el índice especificado y también se puede utilizar para eliminar elementos dentro del rango de índice. También puede eliminar la lista por completo. del lst[índice]: Destrucción de referencia
 
-Eliminar la última empresa de TI de la lista
+#21- Eliminar la mediana empresa de TI o las empresas de la lista.
+#La mediana es el valor que separa la mitad superior de la mitad inferior de un conjunto de datos.
+tam = len(it_companies)//2
+print(it_companies.pop(tam))
 
-Eliminar todas las empresas de TI de la lista
+#22- Eliminar la última empresa de TI de la lista
+print(it_companies.pop())
 
-Destruir la lista de empresas de TI
+#23- Eliminar todas las empresas de TI de la lista
+del it_companies [:] 
+print(it_companies)
 
-Únete a las siguientes listas:
+#24- Destuir la lista de empresas de TI.
+del it_companies
+
+#25- Únete a las siguientes listas:
+#Después de unirse a las listas en la pregunta 26. Copie la lista unida y asigne a una variable full_stack, luego inserte Python y SQL después de Redux.
 
 front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
 back_end = ['Node','Express', 'MongoDB']
 
-    Después de unirse a las listas en la pregunta 26. Copie la lista unida y asigne a una variable full_stack, luego inserte Python y SQL después de Redux.
+full_stack = front_end + back_end #Unir dos listas con el operador +, no modifican las listas originales
+print(full_stack)                 
+lst_copy = full_stack.copy()      # Copiar una lista con el método copy(), lst_copy es una copia de full_stack, si modificamos lst_copy no afecta a full_stack
+lst_copy.append("Python")         # Añadir elementos a la lista copiada
+lst_copy.append("SQL")           
+print(lst_copy)
 
+#El Operador + (Concatenación)
+#Comportamiento: Crea un Nuevo Objeto. Cuando haces lista_nueva = lista_a + lista_b
+# Unir usando el método extend() El método extend() permite añadir la lista en una lista. 
+
+# El Método .extend()
+#Comportamiento: Modifica "In-Place" (Mutación). Cuando haces lista_a.extend(lista_b):
+#   En el Heap: Python va al bloque de memoria de lista_a.
+#    Redimensionamiento (Realloc): Verifica si hay espacio libre al final del bloque de lista_a. Si no lo hay, solicita al sistema operativo agrandar ese mismo bloque.
+#   Copia Parcial: Solo copia los punteros de lista_b y los pega al final de lista_a. Los punteros originales de lista_a no se tocan ni se mueven.
+#    Resultado: lista_a es ahora más grande. No se creó ningún objeto lista nuevo.
+# El método .extend() modifica la lista original "In-place" (en el mismo lugar de la memoria). No crea una lista nueva, sino que expande la que ya existe.  
+
+
+
+"""
 Ejercicios: Nivel 2
 
     La siguiente es una lista de 10 estudiantes de edad:
 
 ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 
-    Ordenar la lista y encontrar la edad mínima y máxima
-    Añadir la edad mínima y la edad máxima de nuevo a la lista
-    Encuentre la edad media (un elemento medio o dos elementos intermedios divididos por dos)
-    Encuentra la edad promedio (suma de todos los artículos divididos por su número)
-    Encuentre el rango de las edades (máximo menos min)
-    Compare el valor de (min - promedio) y (máximo - promedio), use el método abs()
-
-    Encuentra el(los) país(es) medio(s) en la lista de países
-    Divida la lista de países en dos listas iguales si es incluso si no es un país más para la primera mitad.
-    ['China', 'Rusia', 'USA', 'Finlandia', 'Suecia', 'Noruega', 'Dinamarca']. Desempaque los tres primeros países y el resto como países escanédicos.
-
 """
+
+#Ordenar la lista y encontrar la edad mínima y máxima
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+ages.sort() # El método .sort() es una operación "In-place" (en el lugar). Esto significa que modifica la lista original directamente en el Heap y no crea una lista nueva.
+num_max = max(ages)
+num_min = min(ages)
+
+#Añadir la edad mínima y la edad máxima de nuevo a la lista
+ages.append(num_min)
+ages.append(num_max)
+print(ages)
+
+#Encuentre la edad media (un elemento medio o dos elementos intermedios divididos por dos)
+tam = len(ages)
+if tam % 2 == 0:
+    mediana = ages([tam//2]+ages[tam//2-1])/2
+else:
+    mediana = ages[tam//2+1]
+
+print(mediana)
+
+#Encuentra la edad promedio (suma de todos los artículos divididos por su número)
+promedio = sum(ages)/len(ages)
+print(promedio)
+#Encuentre el rango de las edades (máximo menos min)
+rango = num_max - num_min
+
+#Compare el valor de (min - promedio) y (máximo - promedio), use el método abs()
+promedio = sum(ages)/len(ages)
+num_max = max(ages)
+num_min = min(ages)
+distancia_min = abs(num_min - promedio)
+distancia_max = abs(num_max - promedio)
+print(f"Distancia del mínimo: {distancia_min}")
+print(f"Distancia del máximo: {distancia_max}")
+
+# Comparación lógica
+if distancia_min > distancia_max:
+    print("La edad mínima está más lejos del promedio.")
+else:
+    print("La edad máxima está más lejos del promedio.")
+
+
+#Encuentra el(los) país(es) medio(s) en la lista de países.
+paises = ['China', 'Rusia', 'USA', 'Finlandia', 'Suecia', 'Noruega', 'Dinamarca']
+#            0        1       2          3         4          5          6
+tam = len(paises)
+mitad = tam // 2
+
+if tam % 2 != 0:
+    # Caso IMPAR: El centro exacto es 'mitad'
+    print(f"El país medio es: {paises[mitad]}")
+else:
+    # Caso PAR: Los dos centrales son 'mitad - 1' y 'mitad'
+#paises = ['China', 'Rusia', 'USA', 'Finlandia', 'Suecia', 'Noruega', 'Dinamarca', 'Argentina']
+#              0        1      2         3          4          5            6          7
+
+    pais1 = paises[mitad - 1] # 4-1
+    pais2 = paises[mitad] # 4
+    print(f"Los países medios son: {pais1} y {pais2}")
+
+#Divida la lista de países en dos listas iguales si es incluso si no es un país más para la primera mitad.
+paises = ['China', 'Rusia', 'USA', 'Finlandia', 'Suecia', 'Noruega', 'Dinamarca']
+# Tam = 7. El corte será (7 + 1) // 2 = 4
+
+tam = len(paises)
+corte = (tam + 1) // 2  # Este +1 asegura que el "extra" vaya a la primera mitad
+
+primera_mitad = paises[:corte] # Agarra desde el inicio hasta el corte (sin incluir el índice corte)
+segunda_mitad = paises[corte:] # Agarra desde el corte hasta el final
+
+print(f"Primera mitad: {primera_mitad}")
+print(f"Segunda mitad: {segunda_mitad}")
+# Desempaque los tres primeros países y el resto como países escanédicos.
+#['China', 'Rusia', 'USA', 'Finlandia', 'Suecia', 'Noruega', 'Dinamarca']. 
+
+paises = ['China', 'Rusia', 'USA', 'Finlandia', 'Suecia', 'Noruega', 'Dinamarca']
+
+# Desempaquetamos los 3 primeros y el resto en una lista nueva
+p1, p2, p3, *scandic_countries = paises
+
+print(f"País 1: {p1}")
+print(f"País 2: {p2}")
+print(f"País 3: {p3}")
+print(f"Países Escandinavos: {scandic_countries}")
