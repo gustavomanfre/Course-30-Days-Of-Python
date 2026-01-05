e433d1446b52fe5a75f87d15b2b20daa5755afd8
Gustavo Manfredi <gustavomanfredi1995@gmail.com>
2026-01-05 16:55:52 -0300

    Diccionarios

---
+++ Day 08 - Dictionaries/dictionaries.py
@@ -0,0 +1,28 @@
+#💻 Ejercicios: Día 8
+
+# 1-Crear un diccionario vacío llamado perro.
+perro = {}
+
+#2-Añadir nombre, color, raza, piernas, edad al diccionario del perro.
+perro['nombre', 'color', 'raza', 'piernas', 'edad'] =  'Bracko', 'Mixto', 'San Bernardo', 'Largas', 12
+
+#3-Crear un diccionario de estudiantes y añadir first_name, last_name, gender, age, marital status, skills, country, city y address como claves para el diccionario
+estudiantes = {}
+estudiantes['first_name', 'last_name', 'gender', 'age', 'marital status', 'skills', 'country', 'city', 'addres'] = 'Lionel', 'Messi', 'Masculino', 36, 'Casado', ['Regate', 'Tiro Libre'], 'Argentina','Rosario','Valle de las Leñas'
+
+#4-Obtenga la duración del diccionario de estudiantes
+
+#5-Obtenga el valor de las habilidades y verifique el tipo de datos, debería ser una lista
+
+#6-Modificar los valores de las habilidades añadiendo una o dos habilidades
+
+#7-Obtenga las teclas del diccionario como una lista
+
+#8-Obtener los valores del diccionario como una lista
+
+#9-Cambiar el diccionario a una lista de tuplas usando el método items()
+
+#10-Borrar uno de los elementos del diccionario
+
+#11-Borrar uno de los diccionarios
+
