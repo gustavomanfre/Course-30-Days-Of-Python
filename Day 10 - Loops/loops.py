7663c2319f6e62a50b16845d03b3e3ccf7db7e42
Gustavo Manfredi <gustavomanfredi1995@gmail.com>
2026-01-06 16:52:25 -0300

    init loops

---
+++ Day 10 - Loops/loops.py
@@ -0,0 +1,72 @@
+"""
+💻 Ejercicios: Día 10
+
+Ejercicios: Nivel 1
+
+Iterar de 0 a 10 usando para loop, hacer lo mismo usando mientras bucle.
+
+Iterar 10 a 0 usando para loop, hacer lo mismo usando mientras que bucle.
+
+Escriba un bucle que haga siete llamadas a print(), para que tengamos en la salida el siguiente triángulo:
+
+  #
+  ##
+  ###
+  ####
+  #####
+  ######
+  #######
+
+Utilice bucles anidados para crear lo siguiente:
+
+# # # # # # # #
+# # # # # # # #
+# # # # # # # #
+# # # # # # # #
+# # # # # # # #
+# # # # # # # #
+# # # # # # # #
+# # # # # # # #
+
+Imprimir el siguiente patrón:
+
+0 x 0 = 0
+1 x 1 = 1
+2 x 2 = 4
+3 x 3 = 9
+4 x 4 = 16
+5 x 5 = 25
+6 x 6 = 36
+7 x 7 = 49
+8 x 8 = 64
+9 x 9 = 81
+10 x 10 = 100
+
+    Iterar a través de la lista, ['Python', 'Numpy', 'Pandas', 'Django', 'Flask'] usando un bucle para e imprimir los elementos.
+
+    Úselo para loop para iterar de 0 a 100 e imprimir solo números pares
+
+    Úselo para loop para iterar de 0 a 100 e imprimir solo números impares
+
+Ejercicios: Nivel 2
+
+    Utilice para el bucle para iterar de 0 a 100 e imprimir la suma de todos los números.
+
+The sum of all numbers is 5050.
+
+Úselo para loop para iterar de 0 a 100 e imprimir la suma de todos los pares y la suma de todas las probabilidades.
+
+The sum of all evens is 2550. And the sum of all odds is 2500.
+
+Ejercicios: Nivel 3
+
+    Vaya a la carpeta de datos y utilice el archivo countries.py. Atravese los países y extraiga todos los países que contengan la palabra tierra.
+    Esta es una lista de frutas, ['banano', 'naranja', 'mango', 'limón'] invertir el orden usando el bucle.
+    Vaya a la carpeta de datos y utilice la countries_data.py Archivo.
+        ¿Cuál es el número total de idiomas en los datos
+        Encuentra los diez idiomas más hablados a partir de los datos
+        Encuentra los 10 países más poblados del mundo
+
+🎉 ¡FELICITACIONES! 🎉
+
+"""
