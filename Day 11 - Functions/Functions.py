bcd370f955ae97f091d4c03e96b5339829ab1d5b
Gustavo Manfredi <gustavomanfredi1995@gmail.com>
2026-01-07 16:48:02 -0300

    functions

---
+++ Day 11 - Functions/Functions.py
@@ -0,0 +1,68 @@
+
+#💻 Ejercicios: Día 11
+
+#Ejercicios: Nivel 1
+
+"""
+    Declarar una función add_two_numbers. Se necesitan dos parámetros y se devuelve una suma.
+    El área de un círculo se calcula de la siguiente manera: área = π x r x r. Escriba una función que calcule area_of_circle.
+    Escriba una función llamada add_all_nums que toma un número arbitrario de argumentos y suma todos los argumentos. Compruebe si todos los elementos de la lista son tipos de números. Si no, da una retroalimentación razonable.
+    La temperatura en °C se puede convertir a °F usando esta fórmula: °F = (°C x 9/5) + 32. Escriba una función que convierta °C a °F, .
+    Escriba una función llamada check-season, toma un parámetro de mes y devuelve la temporada: otoño, invierno, primavera o verano.
+    Escribir una función llamada calcula_slope que devuelve la pendiente de una ecuación lineal
+    La ecuación cuadrática se calcula como sigue: ax2 + bx + c = 0. Escriba una función que calcule el conjunto de soluciones de una ecuación cuadrática, solve_quadratic_eqn.
+    Declarar una función llamada print_list. Toma una lista como parámetro e imprime cada elemento de la lista.
+    Declarar una función llamada reverse_list. Toma una matriz como parámetro y devuelve lo contrario de la matriz (utiliza bucles).
+
+print(reverse_list([1, 2, 3, 4, 5]))
+# [5, 4, 3, 2, 1]
+print(reverse_list1(["A", "B", "C"]))
+# ["C", "B", "A"]
+
+    Declarar una función llamada capitalize_list_items. Toma una lista como parámetro y devuelve una lista en mayúscula de elementos
+    Declarar una función llamada add_item. Se necesita una lista y un elemento de parámetros. Devuelve una lista con el elemento añadido al final.
+
+food_staff = ['Potato', 'Tomato', 'Mango', 'Milk']
+print(add_item(food_staff, 'Meat'))     # ['Potato', 'Tomato', 'Mango', 'Milk','Meat']
+numbers = [2, 3, 7, 9]
+print(add_item(numbers, 5))      [2, 3, 7, 9, 5]
+
+    Declarar una función llamada remove_item. Se necesita una lista y un elemento de parámetros. Devuelve una lista con el elemento eliminado de ella.
+
+food_staff = ['Potato', 'Tomato', 'Mango', 'Milk']
+print(remove_item(food_staff, 'Mango'))  # ['Potato', 'Tomato', 'Milk'];
+numbers = [2, 3, 7, 9]
+print(remove_item(numbers, 3))  # [2, 7, 9]
+
+    Declarar una función llamada sum_of_numbers. Se necesita un parámetro de número y añade todos los números en ese rango.
+
+print(sum_of_numbers(5))  # 15
+print(sum_of_numbers(10)) # 55
+print(sum_of_numbers(100)) # 5050
+
+    Declarar una función llamada sum_of_odds. Se necesita un parámetro de número y agrega todos los números impares en ese rango.
+    Declarar una función llamada sum_of_even. Se necesita un parámetro de número y agrega todos los números pares en ese - rango.
+
+Ejercicios: Nivel 2
+
+    Declarar una función llamada evens_and_odds . Se necesita un número entero positivo como parámetro y cuenta el número de pares y probabilidades en el número.
+
+    print(evens_and_odds(100))
+    # The number of odds are 50.
+    # The number of evens are 51.
+
+    Llame a su función factorial, toma un número entero como parámetro y devuelve un factorial del número
+    Llama a tu función is_empty, toma un parámetro y comprueba si está vacía o no
+    Escribir diferentes funciones que tomen listas. Deben calcular_media, calcular_median, calcular_modo, calcular_rango, calcular_varianza, calcular_std (desviación estándar).
+
+Ejercicios: Nivel 3
+
+    Escriba una función llamada is_prime, que comprueba si un número es primo.
+    Escriba una función que compruebe si todos los elementos son únicos en la lista.
+    Escriba una función que comprueba si todos los elementos de la lista son del mismo tipo de datos.
+    Escriba una función que compruebe si la variable proporcionada es una variable de Python válida
+    Vaya a la carpeta de datos y acceda al archivo countries-data.py.
+
+    Crear una función llamada los más_hablan_lenguas en el mundo. Debería devolver 10 o 20 idiomas más hablados en el mundo en orden descendente
+    Crear una función llamada most_populed_countries. Debería devolver 10 o 20 países más poblados en orden descendente.
+"""
