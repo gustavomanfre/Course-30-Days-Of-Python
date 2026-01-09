2bb691ef08b9d20eb42d1c2f496a21b87a385755
Gustavo Manfredi <gustavomanfredi1995@gmail.com>
2026-01-08 23:18:26 -0300

    Finalizado Listas por comprension

--- Day 13 - List_comprehension/List_comprehension.py
+++ Day 13 - List_comprehension/List_comprehension.py
@@ -1,18 +1,27 @@
 #💻 Ejercicios: Día 13
 
-#Filtre solo negativo y cero en la lista usando la comprensión de la lista
+#1- Filtre solo negativo y cero en la lista usando la comprensión de la lista
 numbers = [-4,-3,-2,-1,0,2,4,6]
 
 positive_numbers = [i for i in numbers if i >= 0 ]
 print(positive_numbers)
 
-#Filtre la siguiente lista de listas de listas a una lista unidimensional :
-list_of_lists =[[[1, 2, 3]], [[4, 5, 6]], [[7, 8, 9]]]
+#2- Filtre la siguiente lista de listas de listas a una lista unidimensional:
+list_of_lists = [[[1, 2, 3]], [[4, 5, 6]], [[7, 8, 9]]]
 
+# 3- Comprensión de lista para aplanar los 3 niveles
+flattened_list = [number for sublist_externa in list_of_lists for sublist_interna in sublist_externa for number in sublist_interna]
+
+print(flattened_list)
+# Resultado: [1, 2, 3, 4, 5, 6, 7, 8, 9]
 #output
 #[1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-#Usando la comprensión de la lista crear la siguiente lista de tuplas:
+#4- Usando la comprensión de la lista crear la siguiente lista de tuplas:
+numbers = [(i, i**0,i**1,i**2,i**3,i**4, i**5) for i in range(11) ]
+
+numbers = [(i, *(i**j for j in range(6))) for i in range(11)]
+
 """
 [(0, 1, 0, 0, 0, 0, 0),
 (1, 1, 1, 1, 1, 1, 1),
@@ -25,27 +34,39 @@ list_of_lists =[[[1, 2, 3]], [[4, 5, 6]], [[7, 8, 9]]]
 (8, 1, 8, 64, 512, 4096, 32768),
 (9, 1, 9, 81, 729, 6561, 59049),
 (10, 1, 10, 100, 1000, 10000, 100000)]
-"""
-#Aplanar la siguiente lista a una nueva lista:
 
-#countries = [[('Finland', 'Helsinki')], [('Sweden', 'Stockholm')], [('Norway', 'Oslo')]]
+"""
+#5- Aplanar la siguiente lista a una nueva lista:
+countries = [[('Finland', 'Helsinki')], [('Sweden', 'Stockholm')], [('Norway', 'Oslo')]]
+country_list = [[c[0].upper(), c[0][:3].upper(), c[1].upper()] for sub in countries for c in sub]
 
 #output:
 #[['FINLAND','FIN', 'HELSINKI'], ['SWEDEN', 'SWE', 'STOCKHOLM'], ['NORWAY', 'NOR', 'OSLO']]
 
-#Cambie la siguiente lista a una lista de diccionarios:
+#6- Cambie la siguiente lista a una lista de diccionarios:
+countries = [[('Finland', 'Helsinki')], [('Sweden', 'Stockholm')], [('Norway', 'Oslo')]]
+country_list = [ dict([c[0],c[1]]) for sub in countries for c in sub]
 
-#countries = [[('Finland', 'Helsinki')], [('Sweden', 'Stockholm')], [('Norway', 'Oslo')]]
+country_list = [{'country': c[0].upper(), 'city': c[1].upper()} for sub in countries for c in sub]
+print(country_list)
+
 #output:
 #[{'country': 'FINLAND', 'city': 'HELSINKI'},
 #{'country': 'SWEDEN', 'city': 'STOCKHOLM'},
 #{'country': 'NORWAY', 'city': 'OSLO'}]
 
-#Cambie la siguiente lista de listas a una lista de cadenas concatenadas:
+#7-Cambie la siguiente lista de listas a una lista de cadenas concatenadas:
 
-#names = [[('Asabeneh', 'Yetayeh')], [('David', 'Smith')], [('Donald', 'Trump')], [('Bill', 'Gates')]]
+names = [[('Asabeneh', 'Yetayeh')], [('David', 'Smith')], [('Donald', 'Trump')], [('Bill', 'Gates')]]
+names = [t[0] + " " + t[1] for name in names for t in name]
+
 #output
 #['Asabeneh Yetaeyeh', 'David Smith', 'Donald Trump', 'Bill Gates']
 
-#Escriba una función lambda que pueda resolver una pendiente o y-intercept de funciones lineales.
+#8- Escriba una función lambda que pueda resolver una pendiente o y-intercept de funciones lineales.
 
+# Recibe dos tuplas o listas: p1=(x1, y1) y p2=(x2, y2)
+calcular_pendiente = lambda p1, p2: (p2[1] - p1[1]) / (p2[0] - p1[0])
+
+# Ejemplo: puntos (2, 3) y (6, 11)
+print(calcular_pendiente((2, 3), (6, 11)))  # Resultado: 2.0
