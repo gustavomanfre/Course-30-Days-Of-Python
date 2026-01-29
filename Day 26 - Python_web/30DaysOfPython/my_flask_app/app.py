05331a33b2f17454f53bfa90ff721a12f3eeec4d
Gustavo Manfredi <gustavomanfredi1995@gmail.com>
2026-01-28 23:57:16 -0300

    Flask web

--- Day 26 - Python_web/30DaysOfPython/my_flask_app/app.py
+++ Day 26 - Python_web/30DaysOfPython/my_flask_app/app.py
@@ -4,3 +4,12 @@ from flask import Flask
 app = Flask (__name__)
 
 #Ruta principal
+@app.route('/')
+def home():
+    return '<h1>Welcome</h1>'
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug = True, host='0.0.0.0', port=port)
+
+    
